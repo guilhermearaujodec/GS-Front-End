f34c140a2f24278a60e1c6950427ebca0da3fb22
Augusto <auguston22@gmail.com>
2024-06-06 20:55:22 -0300

    Adicionando JavaScript a página login (Alerta e validação do login

---
+++ src/script/login.js
@@ -0,0 +1,64 @@
+// Alerta na página login
+window.onload = function() {
+    alert("Faça o seu login");
+  }
+
+// Disposição para dispositivos mobiles
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "ativo";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".menu-mobile",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+//Validação do login
+function validar(){
+
+    //declarando as variáveis 
+    let usuario =document.getElementById("usuario").value;
+    let senha =document.getElementById("senha").value;
+  
+    if(usuario ==="Admin" && senha ==="12345"){
+        window.open("index.html")                 //retorna para a página home após feito o login
+    }else{
+        alert("usuario e senha inválidos")
+    }
+  }
